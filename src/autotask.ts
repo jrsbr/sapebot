f4f5e6b7f9e9281b73a9b4b941d5f88df7ba5eee
João Azeredo <jsilvadeazeredo@gmail.com>
2026-06-09 15:02:33 -0300

    Adiciona as funções countInWindow, successRate e countDoneByTaskPerson que são auxiliares para a decisão de tarefas automáticas

--- src/autotask.ts
+++ src/autotask.ts
@@ -1,3 +1,5 @@
+import { countReset } from "console";
+import { addDays } from "./time";
 import type { AutoTask, Designated, Designation } from "./types";
 
 function isDesignatedThisDay (
@@ -8,33 +10,45 @@ function isDesignatedThisDay (
     return designateds.some((d) => (date === d.data && task_id === d.task_id));
 }
 
-function isAssignedThisDay (
-    designateds: Designated[],
-    person_id: string,
-    date: string,
-): boolean {}
-
-// terminar
-export function findNextDesignated (
-    designations: Designation[],
-    designateds: Designated[],
-    autoTask: AutoTask[],
-    date: string,
-): Designated {
-    const sortedByCount = designations.map((d) => ({ taskId: d.task_id, name: d.person_id, count: d.count })).sort((a,b) => a.count - b.count);
-    const taskIds  = autoTask.map((a) => a.task_id);
-    let freeTask = ''
-    for (let i in taskIds) {
-        if (isDesignatedThisDay(designateds, i, date)) {
-            freeTask = i;
-            break
-        }
-    }
-}
-
 export function expiredPendingDesignated (
     designateds: Designated[],
     logicalDate: string,
 ): Designated[] {
     return designateds.filter((d) => (d.status === 'pending' && logicalDate > d.data));
+}
+
+export function countInWindow (
+    designateds: Designated[],
+    person_id: string,
+    corte: string,
+): { done: number, missed: number, pending: number } {
+    const personTasks = designateds.filter((d) => d.person_id === person_id && d.data >= corte);
+    const done = personTasks.filter((p) => p.status === 'done').length;
+    const missed = personTasks.filter((p) => p.status === 'missed').length;
+    const pending = personTasks.filter((p) => p.status === 'pending').length;
+    return { done, missed, pending };
+}
+
+export function successRate (
+    done: number,
+    missed: number,
+    pending: number,
+): number {
+    if (done + pending + missed === 0) return 0; // Prioridade máxima para quem não fez nada
+    return (done + pending) / (done + pending + missed)
+}
+
+export function countDoneByTaskPerson (
+    designateds: Designated[],
+    person_id: string,
+    task_id: string,
+    corte: string,
+): number {
+    const total = designateds.filter((d) => 
+        d.person_id === person_id &&
+        d.task_id === task_id &&
+        d.status === 'done' &&
+        d.data >= corte
+        ).length;
+    return total;
 }
